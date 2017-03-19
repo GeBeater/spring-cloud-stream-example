21fe6ff70f1d80cbff28e2843307416995416671
GeBeater <info@gebeat.com>
2017-03-19 14:17:10 +0100

    fix votes resource endpoint issue and usage notice

--- voting-box-service/src/main/java/de/jaxcenter/example/voting/VotingResource.java
+++ voting-box-service/src/main/java/de/jaxcenter/example/voting/VotingResource.java
@@ -2,6 +2,7 @@ package de.jaxcenter.example.voting;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class VotingResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity createVote(Vote vote) {
+    public ResponseEntity createVote(@RequestBody Vote vote) {
         votingService.vote(vote);
         return ResponseEntity.ok().build();
     }
